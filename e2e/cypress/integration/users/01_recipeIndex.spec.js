a9e779dd9e31e74aaf13ee89cd2244cc476dc29f
Dru-Daniels <dancergirl_23@hotmail.com>
2021-01-27 09:35:39 -0500

    fixed indentation on cypress test

--- e2e/cypress/integration/users/01_recipeIndex.spec.js
+++ e2e/cypress/integration/users/01_recipeIndex.spec.js
@@ -11,30 +11,30 @@ describe("Recipes Index", () => {
         description: 'The most delicious, semi healthy snack everrrr!',
         ingredients: 'mixed veggies, soy sauce', 
         instructions: 'put it in a pan until it tastes gud'
-	      }
-      })
-	    cy.visit('/')
-	  })
+      }
+    })
+    cy.visit('/')
+  })
 	
-	  it('has a heading', () => {
-	    cy.get('h1')
-	    .should('have.text', "Top Rated Recipes")
-	  })
-	
-	  it("lists all recipes", () => {
-	    cy.get(".recipe-tile")
-	    .find("h2")
-	    .should("have.text", "Banana Buddies")
-    })
+  it('has a heading', () => {
+    cy.get('h1')
+    .should('have.text', "Top Rated Recipes")
+  })
 
-    it("shows the rating of the recipe", () => {
-      cy.get("p").should("have.text", "Rating: 5 StarsThe most delicious, semi healthy snack everrrr!")
-    })
+  it("lists all recipes", () => {
+    cy.get(".recipe-tile")
+    .find("h2")
+    .should("have.text", "Banana Buddies")
+  })
 
-    it("has a link to go to the recipe show page", () => {
-      cy.get("#recipe-button")
-      .should("have.value", "See Reviews")
-      .click()
-    })
+  it("shows the rating of the recipe", () => {
+    cy.get("p").should("have.text", "Rating: 5 StarsThe most delicious, semi healthy snack everrrr!")
+  })
 
+  it("has a link to go to the recipe show page", () => {
+    cy.get("#recipe-button")
+    .should("have.value", "See Reviews")
+    .click()
+  })
+  
 })
