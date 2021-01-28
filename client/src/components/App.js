4494f7cc45c2cfe26811ace605482d43b5d21633
Maxwell Ayotte <maaxayotte@Maxwells-MacBook-Pro.local>
2021-01-28 14:48:05 -0500

    Nicks requested changes have been fixed!

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { hot } from "react-hot-loader/root";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { hot } from 'react-hot-loader/root';
 
-import getCurrentUser from "../services/getCurrentUser";
-import "../assets/scss/main.scss";
-import RegistrationForm from "./registration/RegistrationForm";
-import SignInForm from "./authentication/SignInForm";
-import TopBar from "./layout/TopBar";
-import RecipeIndex from "./RecipeIndex"
+import getCurrentUser from '../services/getCurrentUser';
+import '../assets/scss/main.scss';
+import RegistrationForm from './registration/RegistrationForm';
+import SignInForm from './authentication/SignInForm';
+import TopBar from './layout/TopBar';
+import RecipeIndex from './RecipeIndex'
 import NewRecipeForm from './NewRecipeForm'
 
 const App = (props) => {
--- client/src/components/NewRecipeForm.js
+++ client/src/components/NewRecipeForm.js
@@ -37,90 +37,86 @@ const handleSubmit = (event) => {
 }
 
 return(
-  <div className='form'>
-    <h1 className= 'formTitle'>Add a new recipe!</h1>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Title:
-        <input
-          type='text'
-          name='title'
-          onChange={handleInputChange}
-          value={newRecipe.title}
-        />
-      </label>
+    <div className='form'>
+      <h1 className= 'formTitle'>Add a new recipe!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Title:
+          <input
+            type='text'
+            name='title'
+            onChange={handleInputChange}
+            value={newRecipe.title}
+          />
+        </label>
 
-      <label>
-        Difficulty:
-        <select name='difficulty' onChange={handleInputChange}>
-          <option></option>
-          <option value='easy'>Easy</option>
-          <option value='intermediate'>Intermediate</option>
-          <option value='advanced'>Advanced</option>
-        </select>
-      </label>
+        <label>
+          Difficulty:
+          <select name='difficulty' onChange={handleInputChange}>
+            <option></option>
+            <option value='easy'>Easy</option>
+            <option value='intermediate'>Intermediate</option>
+            <option value='advanced'>Advanced</option>
+          </select>
+        </label>
 
-      <label>
-        Diet Type:
-        <select name='dietType' onChange={handleInputChange}>
-          <option></option>
-          <option value='vegetarian'>Vegetarian</option>
-          <option value='vegan'>Vegan</option>
-          <option value='meat'>Meat</option>
-        </select>
-      </label>
+        <label>
+          Diet Type:
+          <select name='dietType' onChange={handleInputChange}>
+            <option></option>
+            <option value='vegetarian'>Vegetarian</option>
+            <option value='vegan'>Vegan</option>
+            <option value='meat'>Meat</option>
+          </select>
+        </label>
 
-      <label>
-        Cook Time:
-        <input 
-          type='text'
-          name='cookTime'
-          onChange={handleInputChange}
-          value={newRecipe.cookTime}
-        />
-      </label>
+        <label>
+          Cook Time:
+          <input 
+            type='text'
+            name='cookTime'
+            onChange={handleInputChange}
+            value={newRecipe.cookTime}
+          />
+        </label>
 
-      <label>
-        <textarea 
-          name= 'ingredients'
-          onChange={handleInputChange}
-          value={newRecipe.ingredients}
-          rows= '4'
-          cols= '50'
-          placeholder='Ingredients'
-        />
-      </label>
+        <label>
+          <textarea 
+            name= 'ingredients'
+            onChange={handleInputChange}
+            value={newRecipe.ingredients}
+            rows= '4'
+            cols= '50'
+            placeholder='Ingredients'
+          />
+        </label>
 
-      <label>
-        <textarea 
-          name= 'instructions'
-          onChange={handleInputChange}
-          value={newRecipe.instructions}
-          rows= '8'
-          cols='50'
-          placeholder='Instructions'
-        />
-      </label>
+        <label>
+          <textarea 
+            name= 'instructions'
+            onChange={handleInputChange}
+            value={newRecipe.instructions}
+            rows= '8'
+            cols='50'
+            placeholder='Instructions'
+          />
+        </label>
 
-      <label>
-        URL:
-        <input 
-          type='text'
-          name='url'
-          onChange={handleInputChange}
-          value={newRecipe.url}
-        />
-      </label>
+        <label>
+          URL:
+          <input 
+            type='text'
+            name='url'
+            onChange={handleInputChange}
+            value={newRecipe.url}
+          />
+        </label>
 
-      <div className='button-group'>
-        <input className='button' type='submit' value='Submit' />
-      </div>
-    </form>
-  </div>
-)
+        <div className='button-group'>
+          <input className='button' type='submit' value='Submit' />
+        </div>
+      </form>
+    </div>
+  )
 }
-
-
-
-
 export default NewRecipeForm
