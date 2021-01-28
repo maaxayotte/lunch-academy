7bcb3548b50487c57a0675e4bc23d59419e49a71
Maxwell Ayotte <maaxayotte@Maxwells-MacBook-Pro.local>
2021-01-28 16:53:15 -0500

    Styling for the index bar

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,37 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SignOutButton from "../authentication/SignOutButton";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import SignOutButton from '../authentication/SignOutButton';
 
 const TopBar = ({ user }) => {
   const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link to="/user-sessions/new">Sign In</Link>
+    <li key='sign-in'>
+      <Link to='/user-sessions/new'>Sign In</Link>
     </li>,
-    <li key="sign-up">
-      <Link to="/users/new" className="button">
+    <li key='sign-up'>
+      <Link to='/users/new' className='button'>
         Sign Up
       </Link>
     </li>,
   ];
 
   const authenticatedListItems = [
-    <li key="sign-out">
+    <li key='sign-out'>
       <SignOutButton />
     </li>,
   ];
 
+  function hamburgerMenu() {
+    var burger = document.getElementById("hideToggle");
+    if (burger.style.display === "block") {
+      burger.style.display = "none";
+    } else {
+      burger.style.display = "block";
+    }
+  }
+
   return (
-    <div className="top-bar">
-      <div className="top-bar-left">
-        <ul className="menu">
-          <li className="menu-text">App</li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+    <div className='top-bar' id='top-bar-color'>
+      <div className='top-bar-left' >
+        <ul className='menu' id='top-bar-colors'>
+          <li className='menu-text'></li>
+          <a href="javascript:void(0);" className="icon" onClick={hamburgerMenu}><i className="fa fa-bars"></i></a>
+          <li><Link className="lunch-nav" to='/'>Lunch Academy</Link></li>
+          <div id='hideToggle'>
+            <li className='menu'><Link to='/recipes/new'>Add New Recipe</Link></li>
+          </div>
         </ul>
       </div>
-      <div className="top-bar-right">
-        <ul className="menu">{user ? authenticatedListItems : unauthenticatedListItems}</ul>
+      <div className='top-bar-right'>
+        <ul className='menu'>{user ? authenticatedListItems : unauthenticatedListItems}</ul>
       </div>
     </div>
   );
