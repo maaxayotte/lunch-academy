bdfeb0d89551e95f296df4303ded9eebe42f0323
joshkbutts <joshuabutts37@gmail.com>
2021-02-03 10:22:40 -0500

    getting review forms up to date

--- server/src/db/migrations/20210126120949_createRecipes.cjs
+++ server/src/db/migrations/20210126120949_createRecipes.cjs
@@ -6,20 +6,6 @@
  * @param {Knex} knex
  */
 exports.up = async (knex) => {
-<<<<<<< HEAD
-  return knex.schema.createTable("recipes", (table) => {
-    table.bigIncrements("id")
-    table.string("name").notNullable().unique()
-    table.integer("rating")
-    table.string("cookTime").notNullable()
-    table.string("description")
-    table.text("ingredients").notNullable()
-    table.text("instructions").notNullable()
-    table.string("imageFile")
-    table.string("url")
-    table.timestamp("createdAt").notNullable().defaultTo(knex.fn.now())
-    table.timestamp("updatedAt").notNullable().defaultTo(knex.fn.now())
-=======
   return knex.schema.createTable('recipes', (table) => {
     table.bigIncrements('id')
     table.string('name').notNullable().unique()
@@ -32,7 +18,6 @@ exports.up = async (knex) => {
     table.string('url')
     table.timestamp('createdAt').notNullable().defaultTo(knex.fn.now())
     table.timestamp('updatedAt').notNullable().defaultTo(knex.fn.now())
->>>>>>> 7a4ea239d03f3ca602f6fdf1b0f5313778560ac8
   })
 }
 
